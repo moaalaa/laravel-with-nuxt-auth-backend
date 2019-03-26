e2eac5ddab24d965b67ab75044ecfb77c5acec86
moaalaa <moaalaa16@gmail.com>
2019-03-26 20:07:20 +0200

    Implementging Logout route and Export Collection

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 Route::namespace('Auth')->group(function () {
     Route::post('login', 'LoginController@login');
     Route::post('register', 'RegisterController@register');
+    Route::post('logout', 'LogoutController@logout');
     Route::get('me', 'MeController@me');
 
 });
